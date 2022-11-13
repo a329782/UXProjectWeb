2887395a6fb302b814ef66fcafb23b6f1fc24b75
Humberto Navarro <a320669@uach.mx>
2022-11-12 21:37:59 -0600

    Se modifica menu lateral

--- UXProjectWeb/src/app/app-routing.module.ts
+++ UXProjectWeb/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'inicio',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
