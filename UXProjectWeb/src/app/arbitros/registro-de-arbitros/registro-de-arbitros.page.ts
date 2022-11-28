d34b622c9fe336fe4e445b22c3b3335f8307e893
Humberto Navarro <a320669@uach.mx>
2022-11-28 09:16:50 -0600

    Se crea y modifica pantalla de registro de arbitros

--- UXProjectWeb/src/app/arbitros/registro-de-arbitros/registro-de-arbitros.page.ts
+++ UXProjectWeb/src/app/arbitros/registro-de-arbitros/registro-de-arbitros.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActionSheetController, IonContent, IonSlides, NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-de-arbitros',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroDeArbitrosPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonContent, { static: true }) ionContent: IonContent;
+  @ViewChild(IonSlides, { static: false }) ionSlides: IonSlides;
+  @ViewChild('registerSlider') registerSlider;
+
+  public slidesOpts = {
+    allowTouchMove: false,
+    autoHeight: true,
+  };
+
+  public isBeginning: boolean = true;
+  public isEnd: boolean = false;
+
+  public slideOneForm: FormGroup;
+
+  public submitAttempt: boolean = false;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.slideOneForm = formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      mail: ['', Validators.required],
+      phone: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  next() {
+    //if (this.slideOneForm.valid) {
+      this.registerSlider.slideNext();
+      this.isBeginning = false;
+      this.isEnd = true;
+      this.summary();
+    //}
+  }
+
+  back() {
+    this.registerSlider.slidePrev();
+    this.isBeginning = true;
+    this.isEnd = false;
+  }
+
+  summary(){
+    let name = document.getElementById("sumName").innerHTML = `${this.slideOneForm.value.firstName} ${this.slideOneForm.value.lastName}`;
+    let email = document.getElementById("sumEmail").innerHTML = this.slideOneForm.value.mail;
+    let phone = document.getElementById("sumPhone").innerHTML = this.slideOneForm.value.phone;
+
+    name = this.slideOneForm.value.firstName + this.slideOneForm.value.lastName;
+    email = this.slideOneForm.value.mail;
+    phone = this.slideOneForm.value.phone;
+  }
+
+  save() {
+    this.submitAttempt = true;
+    console.log("success!")
+    console.log(this.slideOneForm.value.phone);
   }
 
 }
