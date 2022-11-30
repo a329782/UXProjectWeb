d1f741b353580e6a223be51250f10e85125b6cfe
Humberto Navarro <a320669@uach.mx>
2022-11-29 23:36:50 -0600

    Se crea la pantalla de registro de equipos

--- UXProjectWeb/src/app/registro-de-equipos/registro-de-equipos.page.ts
+++ UXProjectWeb/src/app/registro-de-equipos/registro-de-equipos.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActionSheetController, IonContent, IonSlides, NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-de-equipos',
@@ -7,10 +9,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroDeEquiposPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(IonContent, { static: true }) ionContent: IonContent;
+  @ViewChild(IonSlides, { static: false }) ionSlides: IonSlides;
+  @ViewChild('teamRegisterSlider') teamRegisterSlider;
+
+  public slidesOpts = {
+    allowTouchMove: false,
+    autoHeight: true,
+  };
+
+  public isBeginning: boolean = true;
+  public isEnd: boolean = false;
+
+  public slideOneForm: FormGroup;
+  public slideTwoForm: FormGroup;
+
+  public submitAttempt: boolean = false;
+
+  public slides: string[];
+  public currentSlide: string;
+
+  public bold = "color: var(--ion-color-primary); font-weight: bold;";
+  public boldNot = "color: var(--ion-color-medium); font-weight: normal;";
+
+  constructor(public formBuilder: FormBuilder) {
+    this.slideOneForm = formBuilder.group({
+      teamName: ['', Validators.required],
+      league: ['', Validators.required]
+    });
+
+    this.slideTwoForm = formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      mail: ['', Validators.required],
+      phone: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+    this.buildSlides();
+  }
+
+  buildSlides() {
+    const slides = ['Datos del equipo', 'Datos del capitán', 'Revisión de datos'];
+    this.currentSlide = slides[0];
+    this.slides = slides;
+  }
+
+  async onSlidesChanged() {
+    const index = await this.ionSlides.getActiveIndex();
+    this.currentSlide = this.slides[index];
+    this.isBeginning = await this.ionSlides.isBeginning();
+    this.isEnd = await this.ionSlides.isEnd();
+  }
+
+  onSlidesDidChange() {
+    this.ionContent.scrollToTop();
+  }
+
+  next() {
+    this.teamRegisterSlider.slideNext();
+    this.summary();
 
   }
 
+  back() {
+    this.teamRegisterSlider.slidePrev();
+  }
+
+  summary(){
+    document.getElementById("sumTeamName").innerHTML = this.slideOneForm.value.teamName;
+    document.getElementById("sumLeague").innerHTML = this.slideOneForm.value.league;
+
+    document.getElementById("sumName").innerHTML = `${this.slideTwoForm.value.firstName} ${this.slideTwoForm.value.lastName}`;
+    document.getElementById("sumEmail").innerHTML = this.slideTwoForm.value.mail;
+    document.getElementById("sumPhone").innerHTML = this.slideTwoForm.value.phone;
+  }
+
+  save() {
+    this.submitAttempt = true;
+    console.log("success!")
+  }
 }
